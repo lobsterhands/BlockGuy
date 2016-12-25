9a33507807b294cdff0a309422bd82b3eddc9bf1
Lyle Denman <denmanlp@gmail.com>
2016-12-24 19:24:18 -0600

    Add new behavior: GroundSmash

---
+++ Assets/Scripts/Behaviors/GroundSmash.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSmash : AbstractBehavior {
+
+	public ParticleSystem smashParticle;
+
+
+	private float pauseTime = 1f;
+	private float thrust = 500f;
+	private	bool justSmashed;
+
+	// Update is called once per frame
+	void Update () {
+
+		var down = inputState.GetButtonValue (inputButtons [0]);
+		var jump = inputState.GetButtonValue (inputButtons [1]);
+		var jumpHoldTime = inputState.GetButtonHoldTime (inputButtons [1]);
+
+
+		if (!collisionState.isStanding && !justSmashed) {
+			if (down && jump && jumpHoldTime < .01f) {
+				justSmashed = true;
+				StartCoroutine(DoSmashThing ());
+			}
+		}
+
+		if (collisionState.isStanding && justSmashed) {
+			justSmashed = false;
+
+			Vector3 pos = body2d.transform.position;
+			pos.y -= 1f;
+			smashParticle.transform.position = new Vector3 (pos.x, pos.y, pos.z);
+
+		}
+	}
+
+	IEnumerator DoSmashThing() {
+		ToggleSimulate ();
+		smashParticle = ParticleSystem.Instantiate (smashParticle);
+		smashParticle.transform.position = body2d.transform.position;
+		smashParticle.Play (true);
+		yield return new WaitForSeconds (pauseTime);
+		ToggleSimulate ();
+		body2d.AddForce (Vector2.down * thrust);
+		smashParticle.transform.position = body2d.transform.position;
+		smashParticle.Play ();
+	}
+
+	void ToggleSimulate() {
+		body2d.simulated = !body2d.simulated;
+	}
+}
---
+++ Assets/Scripts/Behaviors/SimpleMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour {
+
+	[Header("Movement Variables")]
+	public float speed = 5f;
+	public Buttons[] input;
+	public float jumpForce;
+	public float airResistance;
+	public float gravity;
+
+	[Header("Ground Checkers")]
+	public Transform groundCheckLeft;
+	public Transform groundCheckRight;
+
+	private bool groundedLeft = false;
+	private bool groundedRight = false;
+
+	private Rigidbody2D body2d;
+	private InputState inputState;
+
+	[HideInInspector] 
+	public bool isJumping = false;
+
+	// Use this for initialization
+	void Start () {
+		body2d = GetComponent<Rigidbody2D> ();
+		inputState = GetComponent<InputState> ();
+
+		jumpForce = 400f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		var right = inputState.GetButtonValue (input[(int)Buttons.Right]);
+		var left = inputState.GetButtonValue (input[(int)Buttons.Left]);
+		var velX = speed;
+
+		if (right || left) {
+			velX *= left ? -1 : 1;
+		} else {
+			velX = 0;
+		}
+		body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+
+		if (Input.GetButtonDown ("Jump") && IsPlayerGrounded()) {
+			isJumping = true;
+		}
+	}
+
+	void FixedUpdate() {
+		if (isJumping) {
+			body2d.AddForce (new Vector2 (0f, jumpForce));
+			isJumping = false;
+		}
+	}
+
+	bool IsPlayerGrounded() {
+		groundedLeft = Physics2D.Linecast (transform.position, groundCheckLeft.position, 1 << LayerMask.NameToLayer ("Ground"));
+		groundedRight = Physics2D.Linecast(transform.position, groundCheckRight.position, 1 << LayerMask.NameToLayer("Ground"));
+		return (groundedLeft || groundedRight);
+	}
+}
