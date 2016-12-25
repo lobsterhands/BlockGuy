cc1ce59aadc730cef04efb0dc53e77f852c98882
Lyle Denman <denmanlp@gmail.com>
2016-12-24 19:17:35 -0600

    Add logic for loading next scene when player reaches flag

---
+++ Assets/Scripts/TriggerNextLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerNextLevel : MonoBehaviour {
+
+	LevelLoader levelLoader;
+
+	// Use this for initialization
+	void Start () {
+		levelLoader = GetComponent<LevelLoader> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			SceneManager.LoadScene (LevelLoader.getLevel(), LoadSceneMode.Single);
+		}
+	}
+}
